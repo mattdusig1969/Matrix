6714db463aed3b77be5d278f0683cba45339f567
Matt Dusig <mattdusig@icloud.com>
2025-07-14 12:17:34 -0700

    Remove .next build folder from repo

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/dashboard/generateadcode":["static\u002Fchunks\u002Fpages\u002Fdashboard\u002Fgenerateadcode.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fdashboard\u002Fgenerateadcode"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
